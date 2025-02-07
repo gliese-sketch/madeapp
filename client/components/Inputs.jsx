cec0b02acca1133910f2269ebef95f16368817f0
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-07 09:36:21 +0000

    add hidden input upload functionality

--- client/components/Inputs.jsx
+++ client/components/Inputs.jsx
@@ -1,19 +1,26 @@
+import { useRef, useState } from "react";
 import { Button, Input } from "@heroui/react";
-import { SendHorizonalIcon } from "lucide-react";
-import { useState } from "react";
+import { ImageUpIcon, SendHorizonalIcon } from "lucide-react";
 
 export default function Inputs() {
   const [input, setInput] = useState("");
+  const imageEl = useRef(null);
 
   const onSubmit = (e) => {
     e.preventDefault();
+
+    if (!input) {
+      imageEl.current.click();
+    }
+
     console.log(input);
+
     setInput("");
   };
 
   return (
     <form
-      className="absolute bottom-0 left-0 w-full flex gap-0 sm:gap-1 mb-0 sm:mb-2 px-0 sm:px-8 max-w-5xl"
+      className="absolute bottom-0 w-full flex gap-0 sm:gap-1 mb-0 sm:mb-2 px-0 sm:px-8 max-w-6xl left-1/2 -translate-x-1/2"
       onSubmit={onSubmit}
     >
       <Input
@@ -23,8 +30,17 @@ export default function Inputs() {
         onChange={(e) => setInput(e.target.value)}
         autoComplete="off"
       />
+
+      <input
+        type="file"
+        name="file"
+        accept="image/png, image/webp, image/jpeg"
+        ref={imageEl}
+        hidden
+      />
+
       <Button type="Submit" className="bg-blue-100">
-        <SendHorizonalIcon />
+        {input ? <SendHorizonalIcon /> : <ImageUpIcon />}
       </Button>
     </form>
   );
