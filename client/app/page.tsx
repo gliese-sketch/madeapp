0c9d2915df4d647c9bcb3e77c8867df8de073197
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-07 07:08:12 +0000

    add hero UI

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,3 +1,13 @@
+"use client";
+import { Button, HeroUIProvider } from "@heroui/react";
+
 export default function Home() {
-  return <h1>MADE Room</h1>;
+  return (
+    <HeroUIProvider>
+      <div className="min-h-screen max-h-screen grid place-items-center">
+        <h1>MADE Room</h1>
+        <Button>Click me</Button>
+      </div>
+    </HeroUIProvider>
+  );
 }
