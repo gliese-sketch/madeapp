270ea0fb71fdd8700f945cf5d1954c7f2a69ee3d
gliese-sketch <173663566+gliese-sketch@users.noreply.github.com>
2025-02-09 13:01:49 +0000

    add autoscroll and hide the scroll bar

--- client/components/Messages/Messages.jsx
+++ client/components/Messages/Messages.jsx
@@ -1,8 +1,17 @@
+import { useEffect, useRef } from "react";
 import Chat from "./Chat";
 
 export default function Messages({ messages, id }) {
+  const scrollRef = useRef(null);
+
+  useEffect(() => {
+    if (scrollRef.current) {
+      scrollRef.current.scrollIntoView({ behaviour: "smooth" });
+    }
+  }, [messages]);
+
   return (
-    <div className="container mx-auto pt-5 px-3 max-h-[90vh] overflow-scroll">
+    <div className="container mx-auto pt-5 min-h-[90vh] max-h-[90vh] overflow-scroll scrollbar-hidden px-5 py-3">
       <section className="flex gap-1 flex-col">
         {messages.map((message, index) => (
           <Chat
@@ -14,6 +23,8 @@ export default function Messages({ messages, id }) {
           />
         ))}
       </section>
+
+      <div className="auto-scroll" ref={scrollRef}></div>
     </div>
   );
 }
